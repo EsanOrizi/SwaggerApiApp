922fa222c3e5242dd348152c45a18838f52ca89b
Esan Alorizi <62729091+EsanOrizi@users.noreply.github.com>
2023-12-12 19:36:58 +0000

    Customize Swagger

--- SwaggerApi/Program.cs
+++ SwaggerApi/Program.cs
@@ -1,9 +1,20 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opts =>
+{
+    opts.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Our User API",
+        Description = "This Is The Description about our API",
+        
+    });
+});
 
 var app = builder.Build();
 
